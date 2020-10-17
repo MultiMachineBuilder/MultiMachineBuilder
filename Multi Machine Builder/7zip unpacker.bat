33037f6346f34208dc8d17e9512ad3c43ef3a3aa
Monniasza <37037712+Monniasza@users.noreply.github.com>
2020-10-17 10:47:05 +0200

    Create 7zip unpacker.bat

---
+++ Multi Machine Builder/7zip unpacker.bat
@@ -0,0 +1,5 @@
+echo Make sure that you have 7za.exe or 7z.exe file in your user directory.
+pause
+
+7za x "UNPACK THIS HERE, IN THE MAIN DIRECOTRY.zip"
+7z x "UNPACK THIS HERE, IN THE MAIN DIRECOTRY.zip"
