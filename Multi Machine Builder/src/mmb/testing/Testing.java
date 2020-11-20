6c8306a29d531cae028c9a9dfa09dd71d74732a3
Monniasza <37037712+Monniasza@users.noreply.github.com>
2020-11-20 15:05:35 +0100

    mp3 failure

--- Multi Machine Builder/src/mmb/testing/Testing.java
+++ Multi Machine Builder/src/mmb/testing/Testing.java
@@ -2,6 +2,12 @@ package mmb.testing;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.awt.EventQueue;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,39 +15,36 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import mmb.DATA.file.AdvancedFile;
+import mmb.SOUND.MP3Loader;
 import mmb.WORLD.inventory.ItemInventory;
 import mmb.WORLD.inventory.SimpleItem;
 import mmb.debug.Debugger;
 
 class Testing {
+	private static Debugger debug = new Debugger("MP3");
 
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
+	
+	public static void main(String[] args) {
 		
-	}
-
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
+		File file = new File("sound/error.mp3");
+		InputStream is;
+		try {
+			is = new FileInputStream(file);
+			MP3Loader mp3l = new MP3Loader(is);
+			mp3l.start();
+			EventQueue.invokeLater(() -> {
+			    try {
+					mp3l.untilLoad();
+				} catch (InterruptedException e) {}
+			    mp3l.getClip().start();
+			    try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {}
+			});
+		} catch (FileNotFoundException e1) {
+			debug.pstm(e1, "Failed to load MP3 file");
+		}
 		
-	}
-
-	@BeforeEach
-	void setUp() throws Exception {
-	}
-
-	@AfterEach
-	void tearDown() throws Exception {
-	}
-
-	@Test
-	void test() {
-		Debugger debug = new Debugger("TEST");
-		String name = "brokenSoundData/sound/song.mp3";
-		String[] tmp = AdvancedFile.baseExtension(name);
-		String ext = tmp[1];
-		String base = tmp[0];
-		debug.printl(base);
-		debug.printl(ext);
 	}
 
 }
