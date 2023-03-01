0e5cac9bb0345624befb5c857650a9dde92c9431
Monniasza <37037712+Monniasza@users.noreply.github.com>
2023-03-01 14:38:19 +0100

    moved deprecated electric package

--- src/mmb/content/electric/old/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Old, deprecated machines. SOME HAVE ALTERNATIVES
- * @author oskar
- * @deprecate Some machines have alternatives. Check descriptions of them
- */
-@mmb.DeprecatedExtra(replacementVer = "0.5, 0.6", removal = "0.7")
-@Deprecated(forRemoval = false, since = "0.5")
-@javax.annotation.ParametersAreNonnullByDefault
-package mmb.content.electric.old;
