b8ceb4bb2ce716c4dfae03cc01501db9f6aa010b
Monniasza <37037712+Monniasza@users.noreply.github.com>
2026-01-21 11:58:59 +0100

    deleted useless shop panel

--- src/mmb/menu/main/MainMenu.java
+++ src/mmb/menu/main/MainMenu.java
@@ -130,7 +130,6 @@ public class MainMenu extends MMBFrame {
 		tabbedPane.addTab($res("cgui-saves"), null, PanelSaves.INSTANCE, null);
 		tabbedPane.addTab($res("cgui-mods"), null, PanelMods.INSTANCE, null);
 		tabbedPane.addTab($res("cgui-settings"), null, new PanelSettings(), null);
-		tabbedPane.addTab($res("cgui-shop"), new PanelShop());
 		
 		//Check for updates
 		Thread thread = new Thread(this::check4updates);
