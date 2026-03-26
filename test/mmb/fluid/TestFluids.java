5ca40576fb4285d2237f46063bf108a951b15695
Monniasza <37037712+Monniasza@users.noreply.github.com>
2026-03-26 11:35:50 +0100

    fix test fluids

--- test/mmb/fluid/TestFluids.java
+++ test/mmb/fluid/TestFluids.java
@@ -1,8 +1,25 @@
 package mmb.fluid;
 
+import java.awt.Color;
+
+import mmb.PropertyExtension;
+import mmb.engine.block.Block;
 import mmb.engine.block.Blocks;
+import mmb.engine.blockdrawer.ColorDrawer;
 
 public class TestFluids {
-	public static final Fluid WATER = new Fluid(Blocks.air);
-	public static final Fluid LAVA = new Fluid(Blocks.grass);
+	private static final Block WATER_BLOCK = new Block(
+		"test.water",
+		PropertyExtension.setTexture(new ColorDrawer(Color.BLUE)),
+		PropertyExtension.setTitle("Test Water")
+	);
+
+	private static final Block LAVA_BLOCK = new Block(
+		"test.lava",
+		PropertyExtension.setTexture(new ColorDrawer(Color.ORANGE)),
+		PropertyExtension.setTitle("Test Lava")
+	);
+
+	public static final Fluid WATER = new Fluid(WATER_BLOCK);
+	public static final Fluid LAVA = new Fluid(LAVA_BLOCK);
 }
