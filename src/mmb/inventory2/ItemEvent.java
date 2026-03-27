237d34d092e19dfb680f25aeb89e908a65b80b08
Monniasza <37037712+Monniasza@users.noreply.github.com>
2026-03-27 18:57:05 +0100

    reworked ItemEvent

--- src/mmb/inventory2/ItemEvent.java
+++ src/mmb/inventory2/ItemEvent.java
@@ -2,27 +2,29 @@ package mmb.inventory2;
 
 import java.util.Objects;
 
-import mmb.annotations.Nil;
+import mmb.annotations.NN;
+import mmb.engine.item.ItemEntry;
 import mmb.fluid.FluidEventType;
 
-public record ItemEvent(@Nil SlotState oldState, @Nil SlotState newState, int index) {
+/**
+ * Represents an addition, insertion or extraction of an item
+ */
+public record ItemEvent(@NN ItemEntry item, int before, int after) {
 	/**
-	 * Constructs a new fluid event
-	 * @param oldState old contents of a fluid tank
-	 * @param newState new contents of a fluid tank
-	 * @param index index of element changed
-	 * @throws IllegalArgumentException when oldState == null && newState == null
-	 * @throws IndexOutOfBoundsException when index < 0
+	 * Constructs a new item event
+	 * @throws IllegalArgumentException when quantity < 0
+	 * @throws NullPointerException when item == null
 	 */
 	public ItemEvent{
-		if(oldState == null && newState == null) throw new IllegalArgumentException("Both old and new can't be null together");
-		if(index < 0) throw new IndexOutOfBoundsException("index < 0");
+		Objects.requireNonNull(item, "item is null");
+		if(before < 0) throw new IllegalArgumentException("before < 0");
+		if(after < 0) throw new IllegalArgumentException("after < 0");
+		if(before == 0 && after == 0) throw new IllegalArgumentException("before and after can't be both 0");
 	}
 	
 	public FluidEventType classify() {
-		if(oldState == null) return FluidEventType.ADD;
-		if(newState == null) return FluidEventType.REMOVE;
-		if(Objects.equals(oldState.item(), newState.item())) return FluidEventType.MODIFY;
-		return FluidEventType.REPLACE;
+		if(before == 0) return FluidEventType.ADD;
+		if(after == 0) return FluidEventType.REMOVE;
+		return FluidEventType.MODIFY;
 	}
 }
