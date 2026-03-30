b89608bb1a8f213662938cc1b153e78c66f3bd49
Monniasza <37037712+Monniasza@users.noreply.github.com>
2026-03-30 15:45:00 +0200

    added PowerState to represent contents of a PowerHandler

---
+++ src/mmb/power/PowerState.java
@@ -0,0 +1,53 @@
+package mmb.power;
+
+import mmb.engine.Verify;
+
+/**
+ * Represents a state of a {@link PowerHandler} at an instant. 
+ * @param maxVoltage voltage rating of this power state
+ * @param energy energy stored in this power state
+ * @param joulesPerVolt how much energy is stored per volt
+ * @param maxInsertPerTick maximum insertion rate
+ * @param maxExtractPerTick maximum extraction rate
+ */
+public record PowerState(double maxVoltage, double energy, double joulesPerVolt, double maxInsertPerTick, double maxExtractPerTick) {
+	/**
+	 * Constructs a new PowerState
+	 * @param maxVoltage voltage rating of this power state
+	 * @param energy energy stored in this power state
+	 * @param joulesPerVolt how much energy is stored per volt
+	 * @param maxInsertPerTick maximum insertion rate
+	 * @param maxExtractPerTick maximum extraction rate
+	 * @throws IllegalArgumentException when maxVoltage <= 0
+	 * @throws IllegalArgumentException when energy < 0
+	 * @throws IllegalArgumentException when joulesPerVolt <= 0
+	 * @throws IllegalArgumentException when maxExtractPerTick < 0
+	 * @throws IllegalArgumentException when maxInsertPerTick < 0
+	 */
+	public PowerState{
+		Verify.requirePositive(maxVoltage);
+		Verify.requireNonNegative(energy);
+		Verify.requirePositive(joulesPerVolt);
+		Verify.requireNonNegative(maxInsertPerTick);
+		Verify.requireNonNegative(maxExtractPerTick);
+	}
+	
+	/**
+	 * Dumps the contents of a power handler into a {@link PowerState}
+	 * @param ph source power handler
+	 * @return contents of a given power handler
+	 * @throws IllegalArgumentException when ph.voltRating() <= 0
+	 * @throws IllegalArgumentException when ph.storedJoules() < 0
+	 * @throws IllegalArgumentException when ph.joulesPerVolt() <= 0
+	 * @throws IllegalArgumentException when ph.maxInsertPowerPerTick() < 0
+	 * @throws IllegalArgumentException when ph.maxExtractPowerPerTick() < 0
+	 */
+	public static PowerState dump(PowerHandler ph) {
+		double maxVoltage = ph.voltRating();
+		double energy = ph.storedJoules();
+		double joulesPerVolt = ph.joulesPerVolt();
+		double maxInsertPerTick = ph.maxInsertPowerPerTick();
+		double maxExtractPerTick = ph.maxExtractPowerPerTick();
+		return new PowerState(maxVoltage, energy, joulesPerVolt, maxInsertPerTick, maxExtractPerTick);
+	}
+}
